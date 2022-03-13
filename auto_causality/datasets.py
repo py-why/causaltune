a901eca7affa21f3763f4d14ec8c105876d7cb19
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2022-03-13 17:20:13 +0000

    updated lalonde nsw dataset docu

--- auto_causality/datasets.py
+++ auto_causality/datasets.py
@@ -3,14 +3,14 @@ import numpy as np
 from auto_causality.utils import featurize
 
 
-def lalonde() -> pd.DataFrame:
-    """loads the LaLonde dataset
+def lalonde_nsw() -> pd.DataFrame:
+    """loads the Lalonde NSW dataset
     The dataset described the impact of a job training programme on the real earnings
     of individuals several years later.
     The data consists of the treatment indicator (training yes no), covariates (age, race,
     academic background, real earnings 1976, real earnings 1977) and the outcome (real earnings in 1978)
     See also https://rdrr.io/cran/qte/man/lalonde.html#heading-0 
-    
+
     If used for academic purposes, please consider citing the authors:
     Lalonde, Robert: "Evaluating the Econometric Evaluations of Training Programs," American Economic Review,
     Vol. 76, pp. 604-620
