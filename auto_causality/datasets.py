31141e7ff048b90c213a7abbe07306e75858eebd
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2022-03-13 16:55:02 +0000

    added support for Amazon rating dataset

--- auto_causality/datasets.py
+++ auto_causality/datasets.py
@@ -3,6 +3,62 @@ import numpy as np
 from auto_causality.utils import featurize
 
 
+def amazon_reviews(rating="pos") -> pd.DataFrame:
+    """loads amazon reviews dataset
+    The dataset describes the impact of positive (or negative) reviews for products on Amazon on sales.
+    The authors distinguish between items with more than three reviews (treated) and less than three
+    reviews (untreated). As the rating given by reviews might impact sales, they divide the dataset
+    into products with on average positive (more than 3 starts) or negative (less than three stars)
+    reviews.
+    The dataset consists of 305 covariates (doc2vec features of the review text), a binary treatment
+    variable (more than 3 reviews vs less than three reviews) and a continuous outcome (sales).
+
+    If used for academic purposes, please consider citing the authors:
+    @inproceedings{rakesh2018linked,
+        title={Linked Causal Variational Autoencoder for Inferring Paired Spillover Effects},
+        author={Rakesh, Vineeth and Guo, Ruocheng and Moraffah, Raha and Agarwal, Nitin and Liu, Huan},
+        booktitle={Proceedings of the 27th ACM International Conference on Information and Knowledge Management},
+        pages={1679--1682},
+        year={2018},
+        organization={ACM}
+    }
+    Args:
+        rating (str, optional): choose between positive ('pos') and negative ('neg') reviews. Defaults to 'pos'.
+
+    Returns:
+        pd.DataFrame: dataset with cols "treatment", "y_factual" and covariates "x1" to "x300"
+    """
+    try:
+        assert rating in ["pos", "neg"]
+    except AssertionError:
+        print(
+            "you need to specify which rating dataset you'd like to load. The options are 'pos' or 'neg'"
+        )
+
+    try:
+        import gdown
+    except ImportError:
+        gdown = None
+
+    if gdown:
+        if rating == "pos":
+            url = "https://drive.google.com/file/d/167CYEnYinePTNtKpVpsg0BVkoTwOwQfK/view?usp=sharing"
+        elif rating == "neg":
+            url = "https://drive.google.com/file/d/1b-MPNqxCyWSJE5uyn5-VJUwC8056HM8u/view?usp=sharing"
+        gdown.download(url, "amazon_" + rating + ".csv", fuzzy=True)
+        df = pd.read_csv("amazon_" + rating + ".csv")
+        df.drop(df.columns[[2, 3, 4]], axis=1, inplace=True)
+        df.columns = ["treatment", "y_factual"] + ["x_" + str(i) for i in range(1, 301)]
+        return df
+    else:
+        print(
+            f"""The Amazon dataset is hosted on google drive. As it's quite large, the gdown package is required to download
+            the package automatically. The package can be installed via 'pip install gdown'.
+            Alternatively, you can download it from the following link and store it in the datasets folder:
+            {url}"""
+        )
+
+
 def synth_ihdp() -> pd.DataFrame:
     """loads IHDP dataset
     The Infant Health and Development Program (IHDP) dataset contains data on the impact of visits by specialists
