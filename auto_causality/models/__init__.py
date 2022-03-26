f84c672d769e670089269c92b9f680a0cfd4f15c
EgorKraevTransferwise <egor.kraev@transferwise.com>
2022-03-26 21:10:32 +0000

    Fix ate estimation in Dummy model

--- auto_causality/models/__init__.py
+++ auto_causality/models/__init__.py
@@ -3,6 +3,8 @@ from typing import List, Union
 import pandas as pd
 import numpy as np
 
+
+from auto_causality.scoring import ate
 from auto_causality.models.wrapper import DoWhyWrapper, DoWhyMethods
 from auto_causality.models.transformed_outcome import TransformedOutcomeFitter
 
@@ -18,8 +20,17 @@ class Dummy(DoWhyWrapper):
 
 
 class DummyModel(DoWhyMethods):
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(
+        self,
+        propensity_modifiers: List[str],
+        outcome_modifiers: List[str],
+        treatment: str,
+        outcome: str,
+    ):
+        self.propensity_modifiers = propensity_modifiers
+        self.outcome_modifiers = outcome_modifiers
+        self.treatment = treatment
+        self.outcome = outcome
 
     def fit(
         self,
@@ -27,5 +38,6 @@ class DummyModel(DoWhyMethods):
     ):
         pass
 
-    def predict(self, X: Union[np.ndarray, pd.DataFrame]) -> np.ndarray:
-        return np.random.normal(size=(len(X),))
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        mean_, _, _ = ate(X[self.treatment], X[self.outcome])
+        return np.ones(len(X)) * mean_ * (1 + 0.01 * np.random.normal(size=(len(X),)))
