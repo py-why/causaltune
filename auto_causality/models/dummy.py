2805ab02921ef8995b4b04adedcfbc246cfa1fba
EgorKraevTransferwise <egor.kraev@transferwise.com>
2022-12-12 13:16:43 +0000

    Fix our code to match dowhy 0.9 interface changes

--- auto_causality/models/dummy.py
+++ auto_causality/models/dummy.py
@@ -3,70 +3,29 @@ from typing import List, Callable, Any
 import numpy as np
 import pandas as pd
 
-# from auto_causality.models.monkey_patches import PropensityScoreWeightingEstimator
 from auto_causality.models.wrapper import DoWhyMethods, DoWhyWrapper
 from auto_causality.scoring import Scorer
 
-# from auto_causality.scoring import ate
 
-
-# Let's engage in a bit of monkey patching as we wait for this to be merged into DoWhy
-# #TODO: delete this as soon as PR #485 is merged into dowhy
-def effect(self, df: pd.DataFrame, **kwargs) -> np.ndarray:
-    # combining them in this way allows to override method_params from kwargs
-    extra_params = {**self.method_params, **kwargs}
-    new_estimator = type(self)(
-        data=df,
-        identified_estimand=self._target_estimand,
-        treatment=self._target_estimand.treatment_variable,
-        outcome=self._target_estimand.outcome_variable,
-        test_significance=False,
-        evaluate_effect_strength=False,
-        confidence_intervals=False,
-        target_units=self._target_units,
-        effect_modifiers=self._effect_modifier_names,
-        **extra_params,
-    )
-    scalar_effect = new_estimator.estimate_effect()
-    return np.ones(len(df)) * scalar_effect.value
-
-
-# class OutOfSamplePSWEstimator(PropensityScoreWeightingEstimator):
-#     """
-#     A flavor of PSWEstimator that doesn't refit the propensity function
-#     when doing out-of-sample evaluation
-#     """
-#
-#     def __init__(self, *args, recalculate_propensity_score=False, **kwargs):
-#         # for the case when this is not invoked via Econml wrapper,
-#         # need to merge init_args in
-#         init_params = kwargs.pop("init_params", {})
-#         kwargs = {
-#             **kwargs,
-#             **init_params,
-#             "recalculate_propensity_score": recalculate_propensity_score,
-#         }
-#         super().__init__(*args, **kwargs)
-#
-#         # force fitting for the first time
-#         self.recalculate_propensity_score = True
-#         self._estimate_effect()
-#         self.recalculate_propensity_score = recalculate_propensity_score
-#
-#     def effect(self, df: pd.DataFrame, **kwargs):
-#
-#         effect = super().effect(
-#             df,
-#             propensity_model=self.propensity_model,
-#             **kwargs,
-#         )
-#
-#         return effect
-
-# # TODO: delete this once PR #486 is merged in dowhy
-# def _estimate_effect(self):
-#     self._refresh_propensity_score()
-#     return super()._estimate_effect()
+# # Let's engage in a bit of monkey patching as we wait for this to be merged into DoWhy
+# # #TODO: delete this as soon as PR #485 is merged into dowhy
+# def effect(self, df: pd.DataFrame, **kwargs) -> np.ndarray:
+#     # combining them in this way allows to override method_params from kwargs
+#     extra_params = {**self.method_params, **kwargs}
+#     new_estimator = type(self)(
+#         data=df,
+#         identified_estimand=self._target_estimand,
+#         treatment=self._target_estimand.treatment_variable,
+#         outcome=self._target_estimand.outcome_variable,
+#         test_significance=False,
+#         evaluate_effect_strength=False,
+#         confidence_intervals=False,
+#         target_units=self._target_units,
+#         effect_modifiers=self._effect_modifier_names,
+#         **extra_params,
+#     )
+#     scalar_effect = new_estimator.estimate_effect()
+#     return np.ones(len(df)) * scalar_effect.value
 
 
 class DummyModel(DoWhyMethods):
@@ -75,15 +34,15 @@ class DummyModel(DoWhyMethods):
         propensity_modifiers: List[str],
         outcome_modifiers: List[str],
         effect_modifiers: List[str],
-        treatment: str,
-        outcome: str,
+        treatment_name: str,
+        outcome_name: str,
         control_value: Any,
     ):
         self.propensity_modifiers = propensity_modifiers
         self.outcome_modifiers = outcome_modifiers
         self.effect_modifiers = effect_modifiers
-        self.treatment = treatment
-        self.outcome = outcome
+        self.treatment_name = treatment_name
+        self.outcome_name = outcome_name
 
     def fit(
         self,
@@ -92,7 +51,7 @@ class DummyModel(DoWhyMethods):
         pass
 
     def predict(self, X: pd.DataFrame) -> np.ndarray:
-        mean_, _, _ = Scorer.naive_ate(X[self.treatment], X[self.outcome])
+        mean_, _, _ = Scorer.naive_ate(X[self.treatment_name], X[self.outcome_name])
         return np.ones(len(X)) * mean_ * (1 + 0.01 * np.random.normal(size=(len(X),)))
 
 
@@ -102,8 +61,8 @@ class PropensityScoreWeighter(DoWhyMethods):
         propensity_modifiers: List[str],
         outcome_modifiers: List[str],
         effect_modifiers: List[str],
-        treatment: str,
-        outcome: str,
+        treatment_name: str,
+        outcome_name: str,
         propensity_model: Callable,
         min_ps_score: float = 0.05,
         control_value: Any = 0,
@@ -116,30 +75,32 @@ class PropensityScoreWeighter(DoWhyMethods):
             else self.effect_modifiers + self.outcome_modifiers
         )
 
-        self.treatment = treatment
-        self.outcome = outcome
+        self.treatment_name = treatment_name
+        self.outcome_name = outcome_name
         self.propensity_model = propensity_model
         self.min_ps_score = min_ps_score
         self._control_value = control_value
 
     def fit(self, df: pd.DataFrame):
         self._treatment_value = sorted(
-            [v for v in df[self.treatment].unique() if v != self._control_value]
+            [v for v in df[self.treatment_name].unique() if v != self._control_value]
         )
-        self.propensity_model.fit(df[self.propensity_modifiers], df[self.treatment])
+        self.propensity_model.fit(
+            df[self.propensity_modifiers], df[self.treatment_name]
+        )
 
     def predict(self, X: pd.DataFrame):
         p = self.propensity_model.predict_proba(X[self.propensity_modifiers])
         p = np.clip(p, self.min_ps_score, 1 - self.min_ps_score)
         est = np.ones((len(X), len(self._treatment_value)))
         for i, v in enumerate(self._treatment_value):
-            base = X[self.treatment] == self._control_value
-            treat = X[self.treatment] == v
+            base = X[self.treatment_name] == self._control_value
+            treat = X[self.treatment_name] == v
             base_outcome = weighted_average(
-                X.loc[base, self.outcome].values, 1 / p[base, 0]
+                X.loc[base, self.outcome_name].values, 1 / p[base, 0]
             )
             treat_outcome = weighted_average(
-                X.loc[treat, self.outcome].values, 1 / p[treat, 0]
+                X.loc[treat, self.outcome_name].values, 1 / p[treat, 0]
             )
             est[:, i] *= treat_outcome - base_outcome
         return est
--- auto_causality/models/wrapper.py
+++ auto_causality/models/wrapper.py
@@ -25,60 +25,88 @@ class DoWhyMethods:
 class DoWhyWrapper(CausalEstimator):
     def __init__(
         self,
-        data: pd.DataFrame,
         identified_estimand,
-        treatment: str,
-        outcome: str,
-        effect_modifiers: List[str],
         inner_class: Callable,
-        params: dict = None,
-        control_value=0,
-        treatment_value=1,
+        # params: dict = None,
         test_significance=False,
         evaluate_effect_strength=False,
         confidence_intervals=False,
         **kwargs
     ):
-        self._treatment_name = remove_list(treatment)
-        self._outcome_name = remove_list(outcome)
-        self._effect_modifier_names = effect_modifiers
+        self.estimator_class = inner_class
         self._observed_common_causes_names = (
             identified_estimand.get_backdoor_variables().copy()
         )
 
-        params = {} if params is None else params
-        # this is a hack to accomodate different DoWhy versions
-        params = {**params, **kwargs}
+        # params = {} if params is None else params
+        # # this is a hack to accomodate different DoWhy versions
+        # params = {**params, **kwargs}
 
-        self.estimator = inner_class(
-            treatment=self._treatment_name,
-            outcome=self._outcome_name,
-            # TODO: feed through the propensity modifiers where available
-            propensity_modifiers=effect_modifiers + self._observed_common_causes_names,
-            outcome_modifiers=effect_modifiers + self._observed_common_causes_names,
-            effect_modifiers=effect_modifiers,
-            control_value=control_value,
-            **params.get("init_params", {}),
-        )
-
-        self._data = data
         self._significance_test = test_significance
         self._effect_strength_eval = evaluate_effect_strength
         self._confidence_intervals = confidence_intervals
-        self._control_value = control_value
-        self._treatment_value = treatment_value
+
         self._target_estimand = identified_estimand
 
-        self.method_params = params
+        self.method_params = kwargs
         # TODO
         self.symbolic_estimator = ""
         self.effect_intervals = None
 
-    def _estimate_effect(self):
+    def fit(
+        self,
+        data: pd.DataFrame,
+        treatment_name: str,
+        outcome_name: str,
+        effect_modifier_names: List[str],
+        control_value=0,
+    ):
+        self._data = data
+        self._treatment_name = remove_list(treatment_name)
+        self._outcome_name = remove_list(outcome_name)
+        self._effect_modifier_names = effect_modifier_names
 
-        self.estimator.fit(self._data, **(self.method_params.get("fit_params", {})))
+        self.estimator = self.estimator_class(
+            treatment_name=self._treatment_name,
+            outcome_name=self._outcome_name,
+            # TODO: feed through the propensity modifiers where available
+            propensity_modifiers=effect_modifier_names
+            + self._observed_common_causes_names,
+            outcome_modifiers=effect_modifier_names
+            + self._observed_common_causes_names,
+            effect_modifiers=effect_modifier_names,
+            control_value=control_value,
+            **(self.method_params.get("init_params", {})),
+        )
 
-        est = self.estimator.predict(self._data)
+        self.estimator.fit(data, **(self.method_params.get("fit_params", {})))
+
+    def estimate_effect(
+        self,
+        treatment_value=1,
+        control_value=0,
+        target_units="ATE",
+        confidence_intervals=False,
+    ):
+        if isinstance(target_units, pd.DataFrame):
+            data = target_units
+        else:
+            data = self._data
+
+        if confidence_intervals:
+            raise NotImplementedError(
+                "No confidence intervals available for this estimator yet"
+            )
+
+        if isinstance(target_units, str) and target_units.lower() != "ate":
+            raise NotImplementedError(
+                "Only 'ate' and dataframe target units supported at the moment"
+            )
+
+        self._control_value = control_value
+        self._treatment_value = treatment_value
+
+        est = self.estimator.predict(data)
 
         estimate = CausalEstimate(
             estimate=np.mean(est, axis=0),
@@ -88,9 +116,9 @@ class DoWhyWrapper(CausalEstimator):
             realized_estimand_expr=self.symbolic_estimator,
             cate_estimates=est,
             effect_intervals=self.effect_intervals,
-            _estimator_object=self.estimator,
         )
 
+        estimate.add_estimator(self)
         estimate.interpret = lambda: print("Not implemented yet...")
 
         return estimate
