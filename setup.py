6cc4a22cebbae9e67357842a87d3b6228b403bf6
TDebonoWise <timo.debono@transferwise.com>
2023-05-31 14:05:12 +0100

    remove 3.7 from setup

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     url="https://github.com/py-why/causaltune",
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
